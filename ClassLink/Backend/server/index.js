ad8e1adfd812da6484ca62487cf0fa3851df0306
Kristoffer Lesteberg <edu6194294@agderskolen.no>
2025-08-30 18:27:12 +0200

    Fremskritt på websockets, muligheter for å hente informasjon fra url-en

--- ClassLink/Backend/server/index.js
+++ ClassLink/Backend/server/index.js
@@ -1,9 +1,23 @@
 const http = require('http')
-const {webSocketServer} = require('ws')
+const { WebSocketServer } = require('ws')
+
+const url = require("url")
+
 const server = http.createServer()
 
-const wsServer = new webSocketServer({ server })
+const wsServer = new WebSocketServer({ server })
 const port = 8000
+
+
+wsServer.on("connection", (connection, request) => {
+    //Slik ser connectionen ut i browsern
+    //ws://localhost:8000?username = xxx
+
+     const { username } = url.parse(request.url, true).query
+
+     console.log(username)
+})
+
 
 server.listen(port, () => {
     console.log(`Websocket server is running on port ${port}`)
