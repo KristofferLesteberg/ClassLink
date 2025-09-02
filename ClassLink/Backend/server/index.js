1d7d63f9e0a74e3ae9f8b0889325700c2328bb5f
Kristoffer Lesteberg <edu6194294@agderskolen.no>
2025-09-02 12:41:19 +0200

    Ferdig med server til websockets

--- ClassLink/Backend/server/index.js
+++ ClassLink/Backend/server/index.js
@@ -2,19 +2,61 @@ const http = require('http')
 const { WebSocketServer } = require('ws')
 
 const url = require("url")
+const uuidv4 = require("uuid").v4
 
 const server = http.createServer()
 
 const wsServer = new WebSocketServer({ server })
 const port = 8000
 
+const connections = { }
+const users = { }
+
+const broadcast = () => {
+    Object.keys(connections).forEach(uuid => {
+        const connection = connections[uuid]
+        const message = JSON.stringify(users)
+        connection.send(message)
+    })
+}
+
+
+const handleMessage = (bytes, uuid) => {
+    const message = JSON.parse(bytes.toString())
+    const user = users[uuid]
+
+    user.state = message
+
+    broadcast()
+
+    console.log(`${user.username} updated their state: ${JSON.stringify(user.state)}`)
+}
+
+const handleClose = uuid => {
+    console.log(`${users[uuid].username} disconnected`)
+    delete connections[uuid]
+    delete users[uuid]
+
+    broadcast()
+}
+
 
 wsServer.on("connection", (connection, request) => {
     //Slik ser connectionen ut i browsern
-    //ws://localhost:8000?username = xxx
-
-     const { username } = url.parse(request.url, true).query  
+    //ws://localhost:8000?username=xxx
+     const { username } = url.parse(request.url, true).query
+     const uuid = uuidv4()  
      console.log(username)
+     console.log(uuid)
+
+     connections[uuid] = connection
+     users[uuid] = {
+        username: username,
+        state: { }
+     }
+
+     connection.on("message", message => handleMessage(message, uuid))
+     connection.on("close", () => handleClose(uuid))
 })
 
 
